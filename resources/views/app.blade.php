866f8b3759f6d7c9b429eed29076f21b29f573d0
Cedrine Tille <cedrine.tille@gmail.com>
2025-05-06 16:11:11 +0200

    Update route to show app via AppController

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.vue')
 
-@section('title', 'Test Vue')
+@section('title', 'Histoire interactive')
 
 @section('app')
   @vite(['resources/js/app.js'])
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
+use App\Http\Controllers\AppController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-  return view('app');
-});
+Route::get('/', [AppController::class, 'index']);
+
 
 // Instead of using Sanctum (or something similar) for API authentication,
 // we are using the built-in Laravel session authentication system.
