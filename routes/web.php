1d2b00ec5edb943e5f2ac6e1a40003c9a9c3cd29
Cedrine Tille <cedrine.tille@gmail.com>
2025-05-06 16:50:44 +0200

    Change stuff ?

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AppController::class, 'index']);
 
+//To check if the url get an other Chapiter, it will send the user on the page of Chapitre 1
+// Route::get('/chapter/{id}', [AppController::class, 'index']);
+
 
 // Instead of using Sanctum (or something similar) for API authentication,
 // we are using the built-in Laravel session authentication system.
