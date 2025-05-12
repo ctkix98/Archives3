09a7836ed600729310b93f2db57efa381ad0f388
Cedrine Tille <cedrine.tille@gmail.com>
2025-05-12 16:43:27 +0200

    Add new story

--- database/seeders/StorySeeder.php
+++ database/seeders/StorySeeder.php
@@ -4,51 +4,44 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\File;
+use App\Models\Story;
 use App\Models\Chapter;
 use App\Models\Choice;
-use App\Models\Story;
 
 class StorySeeder extends Seeder
 {
     public function run(): void
     {
-        // Lire le fichier JSON
         $json = File::get(database_path('data/story_data.json'));
         $data = json_decode($json, true);
 
-        dump("Seeder lancé ✅");
-        dump("Story title: " . $data['story']['title']);
-        dump("Chapters count: " . count($data['chapters']));
-        dump("Choices count: " . count($data['choices']));
-
-        // Stoppe ici pour tester
-        dd("Fin test");
-
-        // Créer la story
-        $story = Story::create([
-            'title' => $data['story']['title'],
-            'description' => $data['story']['description'] ?? '',
-        ]);
-
-        // Stock temporaire des IDs
-        $chapterIdMap = [];
-
-        foreach ($data['chapters'] as $chapterData) {
-            $chapter = Chapter::create([
-                'story_id' => $story->id,
-                'title' => $chapterData['title'],
-                'text' => $chapterData['text'],
+        foreach ($data['stories'] as $entry) {
+            // Crée l'histoire
+            $story = Story::create([
+                'title' => $entry['story']['title'],
+                'description' => $entry['story']['description'] ?? '',
             ]);
 
-            $chapterIdMap[$chapterData['id']] = $chapter->id;
-        }
+            $chapterIdMap = [];
 
-        foreach ($data['choices'] as $choiceData) {
-            Choice::create([
-                'chapter_id' => $chapterIdMap[$choiceData['from']],
-                'text' => $choiceData['text'],
-                'target_chapter_id' => $chapterIdMap[$choiceData['target']],
-            ]);
+            // Crée les chapitres
+            foreach ($entry['chapters'] as $chapterData) {
+                $chapter = Chapter::create([
+                    'story_id' => $story->id,
+                    'title' => $chapterData['title'],
+                    'text' => $chapterData['text'],
+                ]);
+                $chapterIdMap[$chapterData['id']] = $chapter->id;
+            }
+
+            // Crée les choix
+            foreach ($entry['choices'] as $choiceData) {
+                Choice::create([
+                    'chapter_id' => $chapterIdMap[$choiceData['from']],
+                    'text' => $choiceData['text'],
+                    'target_chapter_id' => $chapterIdMap[$choiceData['target']],
+                ]);
+            }
         }
     }
 }
