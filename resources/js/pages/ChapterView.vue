882d4d2664d82658fbeb0ed9330ec68a73d732bc
Cedrine Tille <cedrine.tille@gmail.com>
2025-05-12 14:46:23 +0200

    Add button to go back to homepage when story is finished

--- resources/js/pages/ChapterView.vue
+++ resources/js/pages/ChapterView.vue
@@ -1,9 +1,10 @@
 <script setup>
 import { ref, onMounted, watch } from 'vue';
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 
 const chapter = ref(null);
 const route = useRoute();
+const router = useRouter();
 
 const loadChapter = async (id) => {
   const res = await fetch(`/api/v1/chapters/${id}`);
@@ -17,6 +18,10 @@ onMounted(() => {
 watch(() => route.params.id, (newId) => {
   loadChapter(newId);
 });
+
+const goHome = () => {
+  router.push('/home');
+};
 </script>
 
 <template>
@@ -24,14 +29,21 @@ watch(() => route.params.id, (newId) => {
     <h2>{{ chapter.title }}</h2>
     <p>{{ chapter.text }}</p>
 
-    <ul>
+    <ul v-if="chapter.choices.length">
       <li v-for="choice in chapter.choices" :key="choice.id">
         <router-link :to="`/chapter/${choice.target_chapter_id}`">
           {{ choice.text }}
         </router-link>
       </li>
     </ul>
+
+    <!-- ✅ Si aucun choix, afficher bouton retour -->
+    <div v-else>
+      <p>Fin de l'histoire !</p>
+      <button @click="goHome">Retour à l'accueil</button>
+    </div>
   </div>
+
   <div v-else>
     Chargement...
   </div>
