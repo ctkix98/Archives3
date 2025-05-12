f3e46788419fff66fd83f537b0145d94489b2cc8
Cedrine Tille <cedrine.tille@gmail.com>
2025-05-12 11:30:46 +0200

    Fix errors

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="api-base-url" content="{{ url('api/v1') }}">
-    <title>@yield('title')</title>
-  </head>
-  <body>
-    <div id="app"></div>
-    @vite(['resources/js/app.js']) {{-- 👈 ici, Vue peut se monter sur #app --}}
-  </body>
-</html>
+@extends('layouts.vue')
+
+@section('title', 'Histoire interactive')
+
+@section('app')
+  @vite(['resources/js/app.js'])
+@endsection
